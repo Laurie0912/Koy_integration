0fa39a0be84a2785605767f9312c0819202d5e2f
Laurie0912 <l_feuillerat@hetic.eu>
2022-02-19 19:24:33 +0100

    update responsive

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// $(document).ready(function() {
-//     $(window).scroll(function() {
-//         $('.header').css("opacity", 1 -
-//         $(window).scrolltop()/500)})
-//     })
+$(document).ready(function() {
+    $(window).scroll(function() {
+        $('.header').css("opacity", 1 -
+        $(window).scrollTop/500)})
+    })
 
- var header = document.querySelector(".header");
- $(window).onscroll = function () {
-    header.classList.toggle("headertoggle");
-  };
+//  var header = document.querySelector(".header");
+//  $(window).onscroll = function () {
+//     header.classList.toggle("headertoggle");
+//   };
